d1d2dec01817739098ab61e23c65fde692a846b5
sivlvy <vladislavsidenko@ukr.net>
2023-12-23 16:27:27 +0200

    update

--- src/components/helpers/dateForm.js
+++ src/components/helpers/dateForm.js
@@ -1,5 +1,9 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
 
 export function date(arg) {
   return formatDistanceToNow(new Date(arg), { addSuffix: true });
+}
+
+export function dateFormat(args) {
+  return format(new Date(args), 'Pp'); //=> '6. listopad'
 }
